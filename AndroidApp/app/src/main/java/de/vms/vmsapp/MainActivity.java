bba6a10b088990d5adb0b05e6994a327cf623298
smeyer98 <43199203+smeyer98@users.noreply.github.com>
2019-09-24 22:16:52 +0200

    implement drawer - Tut pt2

--- AndroidApp/app/src/main/java/de/vms/vmsapp/MainActivity.java
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/MainActivity.java
@@ -1,14 +1,39 @@
 package de.vms.vmsapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close); //connect the drawer and the toolbar
+        drawer.addDrawerListener(toggle);
+        toggle.syncState(); //for hamburger menu to rotate
+    }
+
+    @Override
+    //use this method to close the drawer
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
