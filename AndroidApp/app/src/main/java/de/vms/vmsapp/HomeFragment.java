b95b4b05f0811eb3c132fb527dc8e78ce188f6a7
smeyer98 <43199203+smeyer98@users.noreply.github.com>
2019-09-28 13:29:56 +0200

    Still some issue
    onCreate is not called when including xml

--- AndroidApp/app/src/main/java/de/vms/vmsapp/HomeFragment.java
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/HomeFragment.java
@@ -1,15 +1,21 @@
 package de.vms.vmsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment { //Fragment, AppCompatActivity
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
---
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/ScanInHomeFragmentActivity.java
@@ -0,0 +1,40 @@
+package de.vms.vmsapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ScanInHomeFragmentActivity extends AppCompatActivity {
+
+    public static TextView tv_result;
+    private Button btn_scan;
+
+    private static final String TAG = "ScanInHomeFragmentActiv";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_in_home_fragment);
+
+        Log.d(TAG, "onCreate: created");
+
+        tv_result = (TextView) findViewById(R.id.tv_result);
+
+        btn_scan = (Button) findViewById(R.id.btn_scan);
+
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScanInHomeFragmentActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
