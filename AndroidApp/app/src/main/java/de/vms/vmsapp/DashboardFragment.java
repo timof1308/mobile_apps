9d3d675eb01f3a56a66b2439b848eaf8c62fa2a3
Timo Fischer <timo.fischer1308@gmail.com>
2019-09-29 16:11:47 +0200

    + added dashboard live data

--- AndroidApp/app/src/main/java/de/vms/vmsapp/DashboardFragment.java
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/DashboardFragment.java
@@ -1,18 +1,115 @@
 package de.vms.vmsapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import de.vms.vmsapp.Models.Equipment;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class DashboardFragment extends Fragment {
+    // UI elements
+    private View view;
+    private TextView activeValueTextView;
+    private TextView planendValueTextView;
+    private TextView totalValueTextView;
+    private TextView companiesValueTextView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        // define view
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        // define elements to manipulate
+        activeValueTextView = (TextView) view.findViewById(R.id.activeValueTextView);
+        planendValueTextView = (TextView) view.findViewById(R.id.planendValueTextView);
+        totalValueTextView = (TextView) view.findViewById(R.id.totalValueTextView);
+        companiesValueTextView = (TextView) view.findViewById(R.id.companiesValueTextView);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // load dashboard data
+        getDashboardData();
+    }
+
+    public void getDashboardData() {
+        AsyncTask<Void, Void, String> asyncTask = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                OkHttpClient client = new OkHttpClient();
+                // prepare request
+                // @TODO: get jwt from local storage
+                Request request = new Request.Builder()
+                        .addHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtb2JpbGVfYXBwc19hcGkiLCJzdWIiOjYsImlkIjo2LCJuYW1lIjoiVGVzdCBVc2VyIiwiZW1haWwiOiJ2bXMud3dpMTdzY2FAZ21haWwuY29tIiwicGFzc3dvcmQiOiI5MzdlOGQ1ZmJiNDhiZDQ5NDk1MzZjZDY1YjhkMzVjNDI2YjgwZDJmODMwYzVjMzA4ZTJjZGVjNDIyYWUyMjQ0Iiwicm9sZSI6MSwidG9rZW4iOm51bGwsImlhdCI6MTU2OTU5MjI0Mn0.R6bRJ21QNe-Er5GnakGQAY7YK1KPbN79gX67huhfzO4")
+                        .url("http://35.184.56.207/api/dashboard")
+                        .build();
+
+                // run request
+                try (Response response = client.newCall(request).execute()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    // return response as string to "onPostExecute"
+                    return response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                if (s != null) {
+                    // LOG response
+                    Log.d("dashboard", s);
+                    try {
+                        // pass to function to display data and render view
+                        loadDashboardData(s);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        asyncTask.execute();
+    }
+
+    public void loadDashboardData(String json_string) throws JSONException {
+        // convert json string to json object
+        JSONObject json = new JSONObject(json_string);
+        activeValueTextView.setText(Integer.toString(json.getInt("active")));
+        planendValueTextView.setText(Integer.toString(json.getInt("planned")));
+        totalValueTextView.setText(Integer.toString(json.getInt("total")));
+        companiesValueTextView.setText(Integer.toString(json.getInt("companies")));
     }
 }
