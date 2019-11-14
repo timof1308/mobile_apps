13eb8c2fba015c84b27e62e2c9d5365d74fe5004
smeyer98 <43199203+smeyer98@users.noreply.github.com>
2019-11-14 22:11:10 +0100

    adaptions
    still crashing; User user is null

--- AndroidApp/app/src/main/java/de/vms/vmsapp/ProfileFragment.java
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/ProfileFragment.java
@@ -1,9 +1,13 @@
 package de.vms.vmsapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,12 +25,27 @@ import com.anychart.enums.HoverMode;
 import com.anychart.enums.Position;
 import com.anychart.enums.TooltipPositionMode;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import de.vms.vmsapp.Models.User;
 
 public class ProfileFragment extends Fragment {
     // ui elements
     private View view;
+    private TextView tv_name;
+    private TextView tv_email;
+    private TextView tv_phone;
+    private ImageView iv_profilePic;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,6 +58,13 @@ public class ProfileFragment extends Fragment {
         // define view
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        tv_name = view.findViewById(R.id.tv_name);
+        tv_email = view.findViewById(R.id.tv_email);
+        tv_phone = view.findViewById(R.id.tv_phone);
+        iv_profilePic = view.findViewById(R.id.iv_profilePic);
+
+        getProfileData(1);
+
         return view;
     }
 
@@ -48,4 +74,70 @@ public class ProfileFragment extends Fragment {
 
         // place actions here ...
     }
+
+    public void getProfileData(int userId) {
+        AsyncTask<Void, Void, String> asyncTask = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                OkHttpClient client = new OkHttpClient();
+                // prepare request
+                // @TODO: get jwt from local storage
+                Request request = new Request.Builder()
+                        .addHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtb2JpbGVfYXBwc19hcGkiLCJzdWIiOjYsImlkIjo2LCJuYW1lIjoiVGVzdCBVc2VyIiwiZW1haWwiOiJ2bXMud3dpMTdzY2FAZ21haWwuY29tIiwicGFzc3dvcmQiOiI5MzdlOGQ1ZmJiNDhiZDQ5NDk1MzZjZDY1YjhkMzVjNDI2YjgwZDJmODMwYzVjMzA4ZTJjZGVjNDIyYWUyMjQ0Iiwicm9sZSI6MSwidG9rZW4iOm51bGwsImlhdCI6MTU2OTU5MjI0Mn0.R6bRJ21QNe-Er5GnakGQAY7YK1KPbN79gX67huhfzO4")
+                        .url("http://35.184.56.207/api/users/" + userId)
+                        .build();
+
+                // run request
+                try (Response response = client.newCall(request).execute()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    // return response as string to "onPostExecute"
+                    return response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                if (s != null) {
+                    // LOG response
+                    Log.d("rooms", s);
+                    try {
+                        //
+                        displayProfileData(processProfileData(s));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        asyncTask.execute();
+
+    }
+
+    public User processProfileData(String json) throws JSONException {
+        User user = new User();
+        JSONObject jo = null;
+        try {
+            jo = new JSONObject(json);
+            user = new User(jo.getInt("id"), jo.getString("name"), jo.getString("email"), jo.getString("password"), jo.getInt("role"), jo.getInt("token"));
+            //visitorId = jo.getInt("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public void displayProfileData(User user) {
+        tv_name.setText(user.getName());
+        tv_email.setText(user.getEmail());
+        //tv_phone
+    }
+
+
 }
