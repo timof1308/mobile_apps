206605cfe99d2fef9be937c14c3cbfb910787692
Timo Fischer <timo.fischer1308@gmail.com>
2020-02-19 11:54:02 +0100

    + get text input on btn click

--- AndroidApp/app/src/main/java/de/vms/vmsapp/LoginActivity.java
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/LoginActivity.java
@@ -2,25 +2,32 @@ package de.vms.vmsapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
+    TextInputEditText text_input_username;
+    TextInputEditText text_input_password;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mToolbar = findViewById(R.id.toolbar);
+        text_input_username = findViewById(R.id.text_input_username);
+        text_input_password = findViewById(R.id.text_input_password);
+        loginBtn = findViewById(R.id.login);
         setSupportActionBar(mToolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -31,6 +38,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         mToolbar.setTitle(R.string.login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = text_input_username.getText().toString();
+                String password = text_input_password.getText().toString();
+                Log.d(username, password);
+            }
+        });
     }
 
     @Override
