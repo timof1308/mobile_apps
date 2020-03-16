e1e7cfce8f41a69a0ca73c6a488020d7682bc576
MariePeppler <55106881+MariePeppler@users.noreply.github.com>
2020-03-16 20:37:12 +0100

    Profile erstellt
    Avatarbild hinzugefügt
    Emailanzeige im Profil abhängig vom Token
    Logoutbutton zusätzlich im Profil

--- AndroidApp/app/src/main/java/de/vms/vmsapp/ProfileFragment.java
+++ AndroidApp/app/src/main/java/de/vms/vmsapp/ProfileFragment.java
@@ -1,11 +1,14 @@
 package de.vms.vmsapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,9 +25,16 @@ public class ProfileFragment extends Fragment {
     private String URL;
     private String TOKEN;
 
+    private TextView tv;
+    private Button buttonProfile;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+
+
+
     }
 
     @Nullable
@@ -33,13 +43,44 @@ public class ProfileFragment extends Fragment {
         // define view
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+
         // get api url and token from shared pref
         SharedPreferences shared_pref = getActivity().getSharedPreferences("app", Context.MODE_PRIVATE);
         URL = shared_pref.getString("URL", null);
         TOKEN = shared_pref.getString("token", null);
 
+        User user = null;
+        try {
+            user = JwtController.decodeJwt(TOKEN);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        tv = (TextView) view.findViewById(R.id.profileMail);
+        tv.setText(user.getEmail());
+
+        buttonProfile = (Button) view.findViewById(R.id.buttonProfile);
+
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), WelcomeActivity.class);
+                SharedPreferences.Editor shared_pref_edit = shared_pref.edit();
+                shared_pref_edit.putString("token", null);
+                shared_pref_edit.apply();
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
         return view;
     }
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
